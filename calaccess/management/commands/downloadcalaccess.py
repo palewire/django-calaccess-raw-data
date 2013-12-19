1ffe8e17bdcb30923b07355fcd4be8b0e146cd59
armendariz <aarmendariz@gmail.com>
2013-12-19 09:41:13 -0800

    added a try at date cleaning and load to management command

--- calaccess/management/commands/downloadcalaccess.py
+++ calaccess/management/commands/downloadcalaccess.py
@@ -7,6 +7,7 @@ from cStringIO import StringIO
 from hurry.filesize import size
 from django.conf import settings
 from optparse import make_option
+from django.db import connection
 from django.utils.six.moves import input
 from csvkit import CSVKitReader, CSVKitWriter
 from dateutil.parser import parse as dateparse
@@ -70,6 +71,19 @@ custom_options = (
     ),
 )
 
+
+def date_clean_csv_line(field_list):
+    csv_line_clean_dates = []
+    for csv_field in field_list:
+        if csv_field != '':
+            try:
+                csv_line_clean_dates.append(dateformat(dateparse(csv_field), 'Y-m-d'))
+            except:
+                csv_line_clean_dates.append(csv_field)
+        else:
+            csv_line_clean_dates.append(csv_field)
+    return csv_line_clean_dates
+    
 
 class Command(BaseCommand):
     help = 'Download the latest snapshot of the CalAccess database'
@@ -188,7 +202,90 @@ class Command(BaseCommand):
         """
         print "Cleaning data files"
         csv.field_size_limit(1000000000)  # Up the CSV data limit
-
+        
+        date_field_dict =  {
+            'CVR_CAMPAIGN_DISCLOSURE_CD': [
+                'ELECT_DATE',
+                'FROM_DATE',
+                'RPT_DATE',
+                'RPTFROMDT',
+                'RPTTHRUDT',
+                'THRU_DATE'
+            ],
+           'CVR_LOBBY_DISCLOSURE_CD': [
+                'CUM_BEG_DT',
+                'FROM_DATE',
+                'RPT_DATE',
+                'SIG_DATE',
+                'THRU_DATE'
+            ],
+           'CVR_REGISTRATION_CD': [
+                'COMPLET_DT',
+                'EFF_DATE',
+                'QUAL_DATE',
+                'RPT_DATE',
+                'SIG_DATE'
+            ],
+           'EXPN_CD': [
+                'EXPN_DATE'
+            ],
+           'FILERNAME_CD': [
+                'EFFECT_DT'
+            ],
+           'FILER_FILINGS_CD': [
+                'FILING_DATE',
+                'RPT_START',
+                'RPT_END',
+                'RPT_DATE'
+            ],
+           'FILER_INTERESTS_CD': [
+                'EFFECT_DATE'
+            ],
+           'FILER_LINKS_CD': [
+                'EFFECT_DT',
+                'TERMINATION_DT'
+            ],
+           'FILER_TO_FILER_TYPE_CD': [
+                'EFFECT_DT',
+                'NYQ_DT'
+            ],
+           'FILER_XREF_CD': [
+                'EFFECT_DT'
+            ],
+           'FILING_PERIOD_CD': [
+                'START_DATE',
+                'END_DATE',
+                'DEADLINE'
+            ],
+           'LEMP_CD': [
+                'EFF_DATE'
+            ],
+           'LEXP_CD': [
+                'EXPN_DATE'
+            ],
+           'LOAN_CD': [
+                'LOAN_DATE1',
+                'LOAN_DATE2'
+            ],
+           'LOBBY_AMENDMENTS_CD': [
+                'ADD_L_EFF',
+                'ADD_LE_EFF',
+                'ADD_LF_EFF',
+                'DEL_LF_EFF',
+                'OTHER_EFF'
+            ],
+           'LOTH_CD': [
+                'PMT_DATE',
+            ],
+           'RCPT_CD': [
+                'DATE_THRU',
+                'RCPT_DATE'
+            ],
+           'SMRY_CD': [
+                'ELEC_DT'
+            ],
+        }
+        
         # Loop through all the files in the source directory
         for name in os.listdir(self.tsv_dir):
             print "- %s" % name
@@ -210,15 +307,81 @@ class Command(BaseCommand):
             csv_file = open(csv_path, 'wb')
             csv_writer = CSVKitWriter(csv_file, quoting=csv.QUOTE_ALL)
             tsv_reader = StringIO(tsv_data)
+            
+            headers = tsv_reader.next()
+            headers = headers.decode("ascii", "replace").encode('utf-8')
+            headers_csv = CSVKitReader(StringIO(headers), delimiter='\t')
+            headers_list = headers_csv.next()
+            csv_writer.writerow(headers_list)
+            
             for tsv_line in tsv_reader:
                 # Goofing around with the encoding while we're in there.
                 tsv_line = tsv_line.decode("ascii", "replace").encode('utf-8')
-                csv_line = CSVKitReader(StringIO(tsv_line), delimiter='\t')
-                csv_writer.writerow(csv_line.next())
-
+                # choking on fields with bad quoting again
+                # eg.  '"HEIGHT AIN\'T RIGHT DOWNTOWN\' (DOWNTOWN HEIGHTS LIMIT INITIATIVE)',
+                # quotes aren't closed
+                try:
+                    csv_line = CSVKitReader(StringIO(tsv_line), delimiter='\t')
+                    #csv_line_date_cleaned = date_clean_csv_line(csv_line.next())
+                    #csv_writer.writerow(csv_line_date_cleaned)
+                    csv_field_list = csv_line.next()
+                except:
+                    tsv_clean_line = ''.join(c for c in tsv_line if c not in ('"', "'")) # so strip all quotes for now
+                    csv_line = CSVKitReader(StringIO(tsv_clean_line), delimiter='\t')
+                    #csv_line_date_cleaned = date_clean_csv_line(csv_line.next())
+                    #csv_writer.writerow(csv_line_date_cleaned)
+                    csv_field_list = csv_line.next()
+                
+                if name.replace('.TSV', '') in date_field_dict:
+                    date_field_list = date_field_dict[name.replace('.TSV', '')]
+                    for f in date_field_list:
+                        if csv_field_list[headers_list.index(f)] != '':
+                            try:
+                                csv_field_list[headers_list.index(f)] = dateformat(dateparse(csv_field_list[headers_list.index(f)]), 'Y-m-d')
+                            except:
+                                print '- INVALID DATE: %s\t%s\t%s' % (name, f, csv_field_list[headers_list.index(f)])
+                                csv_field_list[headers_list.index(f)] = ''
+                csv_writer.writerow(csv_field_list) 
+                
+                
             # Shut it down
             tsv_reader.close()
             csv_file.close()
+        
+        def load(self):
+            for name in os.listdir(self.csv_dir):
+                print "- %s" % name
+                csv_path = os.path.join(
+                    self.csv_dir,
+                    name
+                )
+                
+                cursor = connection.cursor()
+                cursor.execute('SHOW TABLES')
+                table_list = [t[0] for t in cursor.fetchall()]
+                table_dict = {}
+                for table in table_list:
+                    if table ==  name.replace('.csv', '').upper():
+                        table_dict[name] = table
+                
+                load_path = os.path.abspath(csv_path)
+                bulk_sql_load_part_1 = '''
+                    LOAD DATA LOCAL INFILE '%s'
+                    INTO TABLE %s
+                    FIELDS TERMINATED BY ','
+                    OPTIONALLY ENCLOSED BY '"'
+                    IGNORE 1 LINES
+                    (
+                ''' % (load_path, table_dict[name])
+                infile = open(csv_path)
+                csv_reader = CSVKitReader(infile)
+                headers = csv_reader.next()
+                infile.close()
+                sql_fields = ['`%s`' % h for h in headers]
+                bulk_sql_load =  bulk_sql_load_part_1 + ','.join(sql_fields) + ')'
+                
+                cursor.execute('DELETE FROM %s' % table_dict[name])
+                cursor.execute(bulk_sql_load)
 
 
 #"DATE"
