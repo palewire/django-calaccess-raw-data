976e7a0dde2582855c5d42792b2acae3b3ad2155
aboutaaron <aaron.colby.williams@gmail.com>
2014-02-03 14:00:28 -0800

    some dummy view data

--- campaign_finance/views.py
+++ campaign_finance/views.py
@@ -1,1 +1,18 @@
 # Create your views here.
+from django.shortcuts import get_list_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from campaign_finance.models import *
+
+class IndexView(generic.ListView):
+  template_name = 'templates/campaign_finance/index.html'
+  context_object_name = 'latest_campaigns'
+
+  def get_queryset(self):
+    return Committee.objects.order_by('-name')[:5]
+
+class CommitteeView(generic.DetailView):
+  model = Committee
+  template = 'templates/campaign_finance/committee.html'
