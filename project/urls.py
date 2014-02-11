77d0680d532d6c3d6e0bb0f38d8aa396f652781c
aboutaaron <aaron.colby.williams@gmail.com>
2014-02-11 14:43:16 -0800

    added other routes

--- project/urls.py
+++ project/urls.py
@@ -12,6 +12,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^filers/(?P<pk>\d+)/$', views.FilerView.as_view(template_name='filer/detail.html'), name='filer_detail'),
+    url(r'^candidate/(?P<pk>\d+)/$', views.FilerView.as_view(template_name='filer/detail.html'), name='filer_detail'),
+    url(r'^committees/(?P<pk>\d+)/$', views.CommitteeView.as_view(template_name='committee/detail.html'), name='committee_detail'),
     url(r'^search/', include('haystack.urls'), name='search'),
 )
 
