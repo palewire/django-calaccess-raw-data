567dc5e98208b98c6bf553ab3b9e5ec22bae19f6
Aaron Williams <aaron.colby.williams@gmail.com>
2014-04-22 11:44:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-calaccess-parser',
-    version='0.1',
+    version='0.2',
     packages=[
         'calaccess',
         'calaccess.management',
