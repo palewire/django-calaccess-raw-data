c6c7df438df5f8c4f2473f9fcf216fd75db89132
palewire <ben.welsh@gmail.com>
2014-08-06 11:11:43 -0700

    Static URLs fix

--- example/project/urls.py
+++ example/project/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'example.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+        'show_indexes': True,
+    }),
 )
