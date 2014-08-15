7e8782be4ab0cf556f29be32b881125824628e20
palewire <ben.welsh@gmail.com>
2014-08-15 16:54:49 -0700

    Read only admins for #31

--- calaccess_raw/admin/base.py
+++ calaccess_raw/admin/base.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 
 class BaseAdmin(admin.ModelAdmin):
     save_on_top = True
+
+    def get_readonly_fields(self, *args, **kwargs):
+        return [f.name for f in self.model._meta.fields]
