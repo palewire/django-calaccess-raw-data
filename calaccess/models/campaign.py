f4705481c766f38c0afc4f03bb40d93a87a4d18b
palewire <ben.welsh@gmail.com>
2014-08-15 14:04:17 -0700

    Corrected CvrSo model name to CvrSoCd

--- calaccess/models/campaign.py
+++ calaccess/models/campaign.py
@@ -3,7 +3,7 @@ from django.db import models
 from .base import CalAccessBaseModel
 
 
-class CvrSo(CalAccessBaseModel):
+class CvrSoCd(CalAccessBaseModel):
     '''
         Cover page for statement of organization
 
