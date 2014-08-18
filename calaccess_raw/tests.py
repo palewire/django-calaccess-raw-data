dcd14ed39b751ee8cda635cd32babd90a16a2214
palewire <ben.welsh@gmail.com>
2014-08-18 09:31:29 -0700

    Updated tests to new namespace

--- calaccess_raw/tests.py
+++ calaccess_raw/tests.py
@@ -7,7 +7,7 @@ class CalAccessTest(TestCase):
 
     @override_settings(BASE_DIR='example/')
     def test_csv_gettrs(self):
-        model_list = get_models(get_app("calaccess"))
+        model_list = get_models(get_app("calaccess_raw"))
         for m in model_list:
             self.assertEqual(m.objects.get_csv_name(), m().get_csv_name())
             self.assertEqual(m.objects.get_csv_path(), m().get_csv_path())
