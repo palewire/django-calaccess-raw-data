434ed6506849fbba2ad00e24778d9a80cfa90126
palewire <ben.welsh@gmail.com>
2014-10-01 17:03:57 -0700

    Wrote a new base command that can prettify outputs and put it to use on the our simplest mgmt command. For #57.

---
+++ calaccess_raw/management/commands/__init__.py
@@ -0,0 +1,25 @@
+from django.utils.termcolors import colorize
+from django.core.management.base import BaseCommand
+
+
+class CalAccessCommand(BaseCommand):
+
+    def header(self, string):
+        self.stdout.write(
+            colorize(string, fg="cyan", opts=("bold",))
+        )
+
+    def log(self, string):
+        self.stdout.write(
+            colorize("- %s" % string, fg="white")
+        )
+
+    def success(self, string):
+        self.stdout.write(
+            colorize(string, fg="green", opts=("bold",))
+        )
+
+    def failure(self, string):
+        self.stdout.write(
+            colorize(string, fg="red", opts=("bold",))
+        )
--- calaccess_raw/management/commands/totalcalaccessrawdata.py
+++ calaccess_raw/management/commands/totalcalaccessrawdata.py
@@ -1,15 +1,16 @@
 from calaccess_raw import get_model_list
-from django.core.management.base import BaseCommand
+from calaccess_raw.management.commands import CalAccessCommand
 from django.contrib.humanize.templatetags.humanize import intcomma
 
 
-class Command(BaseCommand):
+class Command(CalAccessCommand):
     help = "Print out the total of CAL-ACCESS tables and rows in the database"
 
     def handle(self, *args, **kwargs):
+        self.header("Totaling CAL-ACCESS tables and row")
         model_list = get_model_list()
-        print "%s total database tables" % len(model_list)
+        self.log("%s total tables" % len(model_list))
         record_count = 0
         for m in model_list:
             record_count += m.objects.count()
-        print "%s total database records" % intcomma(record_count)
+        self.log("%s total records" % intcomma(record_count))
