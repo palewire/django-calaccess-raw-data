ff4dba1e13fe3b9099ff88943b7e68c8055dd722
anabranch <wac.chambers@gmail.com>
2014-11-11 15:48:30 -0800

    Added Lengths where they were required

--- calaccess_raw/models/campaign.py
+++ calaccess_raw/models/campaign.py
@@ -406,7 +406,7 @@ class CvrCampaignDisclosureCd(CalAccessBaseModel):
     filer_fax = models.CharField(
         max_length=20L, db_column='FILER_FAX', blank=True
     )
-    filer_id = models.CharField(db_column='FILER_ID', db_index=True)
+    filer_id = models.CharField(max_length=15L, db_column='FILER_ID', db_index=True)
     filer_namf = models.CharField(
         max_length=45L, db_column='FILER_NAMF', blank=True
     )
@@ -537,6 +537,7 @@ class CvrCampaignDisclosureCd(CalAccessBaseModel):
         max_length=20L, db_column='TRES_PHON', blank=True
     )
     tres_st = models.CharField(max_length=2L, db_column='TRES_ST', blank=True)
+
     tres_zip4 = models.CharField(
         max_length=10L, db_column='TRES_ZIP4', blank=True
     )
@@ -1115,8 +1116,9 @@ address"
     )
     tres_zip4 = models.CharField(
         null=True,
-        db_column='TRES_ZIP4',
+        max_length=10L,
         blank=True,
+        db_column='TRES_ZIP4',
         help_text="Zip code portion of the treasurer or responsible officer's \
 address"
     )
