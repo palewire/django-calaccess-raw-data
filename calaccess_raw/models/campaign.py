4a8aa36ced5f73d2cd5ccf4fb658ab4a3dd9d148
joannalin <jlin@cironline.org>
2015-03-11 15:08:44 -0700

    Added help_text for memo_refno
    lines 1736-1740

--- calaccess_raw/models/campaign.py
+++ calaccess_raw/models/campaign.py
@@ -1908,7 +1908,10 @@ class ExpnCd(CalAccessBaseModel):
         max_length=1L, db_column='MEMO_CODE', blank=True, help_text="Memo Amount? (Date/Amount are informational only)"
     )
     memo_refno = models.CharField(
-        max_length=20, db_column='MEMO_REFNO', blank=True
+        max_length=20L,
+        db_column='MEMO_REFNO',
+        blank=True,
+        help_text="Reference to text contained in a TEXT record."
     )
     off_s_h_cd = models.CharField(
         max_length=1, db_column='OFF_S_H_CD', blank=True
