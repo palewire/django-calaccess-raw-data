a90a90a5dd1a5ec890ff52b73adb3e6ed1693491
palewire <ben.welsh@gmail.com>
2015-03-23 16:44:49 -0700

    Better name

--- calaccess_raw/tests.py
+++ calaccess_raw/tests.py
@@ -24,28 +24,33 @@ class CalAccessTest(TestCase):
             m().__str__()
             self.assertNotEqual(m().__str__(), '%s object' % m.__name__)
 
-    def test_form_type_fields(self):
+    def test_form_type_choices(self):
         """
         Verify that proper choices appear for all form_type fields.
         """
         # Loop through the models
         for m in get_model_list():
+
             # And then through the fields
             for f in m._meta.fields:
+
                 # Only test against the ones called form_type
-                if f.name == 'form_type':
-                    # Pull out all the choices in that field
-                    slug_list = []
-                    for slug, name in f.choices:
-                        # Make sure that each has a definition
-                        # self.assertIsNot(name, '')
-                        if not name:
-                            warnings.warn("%s form_type '%s' undefined" % (
-                                m.__name__,
-                                slug,
-                            ))
-                        slug_list.append(slug)
-                    # The query the database and make sure everything in
-                    # there has a matching definition in the choices
-                    for v in m.objects.values("form_type").distinct():
-                        self.assertIn(v, slug_list)
+                if not f.name == 'form_type':
+                    continue
+
+                # Pull out all the choices in that field
+                slug_list = []
+                for slug, name in f.choices:
+                    # Make sure that each has a definition
+                    # self.assertIsNot(name, '')
+                    if not name:
+                        warnings.warn("%s form_type '%s' undefined" % (
+                            m.__name__,
+                            slug,
+                        ))
+                    slug_list.append(slug)
+
+                # The query the database and make sure everything in
+                # there has a matching definition in the choices
+                for v in m.objects.values("form_type").distinct():
+                    self.assertIn(v, slug_list)
