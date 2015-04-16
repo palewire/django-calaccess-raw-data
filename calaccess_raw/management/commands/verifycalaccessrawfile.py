56f2f482e3f5cfe6660f6c51d456619ea40b629b
Ray Kiddy <ray@ganymede.org>
2015-04-15 20:30:26 -0700

    Now checks for non-ascii characters and uses a --create-diff option.

--- calaccess_raw/management/commands/verifycalaccessrawfile.py
+++ calaccess_raw/management/commands/verifycalaccessrawfile.py
@@ -1,17 +1,34 @@
 from __future__ import unicode_literals
 from django.db import connection
-from django.db.models import Max
+from optparse import make_option
 from django.db.models.loading import get_model
 from django.core.management.base import LabelCommand
 from calaccess_raw.management.commands import CalAccessCommand
 
 
 class Command(CalAccessCommand, LabelCommand):
-    help = 'Compare the number of records in a model against its source CSV'
+
+    option_list = LabelCommand.option_list + (
+        make_option(
+            '--create-diff',
+            action="store_true",
+            dest="create_diff",
+            help="Create a difference file for the TSV and the database."
+        ),
+    )
+
+    help = 'Compare the number of records in a model \
+against its source CSV and TSV and show the missing ids. \
+\
+Using -create-diff will generate a new copy of the \
+TSV file from the database table, thus showing what \
+was missed.'
     args = '<model name>'
 
     def handle_label(self, label, **options):
         self.log(" Verifying %s" % label)
+
+        create_diff = options['create_diff']
 
         # Get the model
         model = get_model("calaccess_raw", label)
@@ -21,45 +38,66 @@ class Command(CalAccessCommand, LabelCommand):
         csv_cnt = sum(1 for line in open(model.objects.get_csv_path())) - 1
         db_cnt = model.objects.count()
 
-        # Read the highest id value from the files and the database
-        high_tsv_id = model.objects.get_highest_tsv_id()
-        high_csv_id = model.objects.get_highest_csv_id()
-        high_db_id = int(model.objects.all().aggregate(Max('id'))['id__max'])
-
-        if tsv_cnt == csv_cnt == db_cnt == high_tsv_id == high_csv_id == high_db_id:
-            self.success('  Table record counts and high id values matches in CSV, TSV, and database')
+        if tsv_cnt == csv_cnt == db_cnt:
+            self.success('  Table record counts match in CSV, TSV, and db')
         else:
-            self.failure('  Table Record counts or high id values in TSV, CSV or database.')
-            self.failure('    table # %d, CSV # %d, TSV # %d' % (db_cnt, csv_cnt, tsv_cnt))
+            self.failure('  Table Record counts NOT match in TSV, CSV or db.')
+            self.failure('    table # %d, CSV # %d, TSV # %d' %
+                         (db_cnt, csv_cnt, tsv_cnt))
 
-            # Regenerate the TSV file. Since there was a problem, one should see differences.
+            # If the -create-diff option is set, create a new version of the
+            # TSV file from the database table.
+            #
+            # If not, just get the ids from the table and check against the
+            # TSV file to find the missing ids.
+            #
+            next_ids = []
+
+            if create_diff:
+
+                with open(model.objects.get_tsv_path()) as f:
+                    col_names = f.readline().replace('\r\n', '')
+
+                next_file_name = model.objects.get_tsv_path().replace(
+                    '.TSV',
+                    '_DERIVED.TSV')
+
+                next_file = open(next_file_name, 'w')
+                next_file.write(col_names + '\r\n')
+
+            for row in model.objects.all():
+                next_ids.append(str(row.id))
+
+                if create_diff:
+
+                    next_row = []
+
+                    for col_name in col_names.split('\t'):
+
+                        field_name = col_name.lower()
+                        if field_name == 'hdrcomment':
+                            field_name = 'hdr_comment'
+
+                        next_row.append(str(getattr(row, field_name)))
+
+                    next_file.write('\t'.join(next_row) + '\r\n')
+
+            if create_diff:
+                next_file.close()
+                self.failure('    compare: %s %s' %
+                             (model.objects.get_tsv_path(), next_file_name))
+
+            # Find the ids that are missing from the database
             #
             with open(model.objects.get_tsv_path()) as f:
-                col_names = f.readline().replace('\r\n', '')
+                f.readline()
+                original_ids = []
+                for line in f:
+                    try:
+                        original_ids.append(str(line.split('\t')[0]))
+                    except UnicodeDecodeError:
+                        self.failure('    UnicodeDecodeError: "%s"' %
+                                     unicode(line, 'utf-8'))
 
-            next_file_name = model.objects.get_tsv_path().replace('.TSV', '_DERIVED.TSV')
-
-            next_file = open(next_file_name, 'w')
-            next_file.write(col_names + '\r\n')
-
-            # I need to do this because I need the values in the same order as the TSV.
-            #
-            # The field name is the lower-case version of the column key. Except
-            # when it is not.
-            #
-            for row in model.objects.all():
-
-                next_row = []
-
-                for col_name in col_names.split('\t'):
-
-                    field_name = col_name.lower()
-                    if field_name == 'hdrcomment':
-                        field_name = 'hdr_comment'
-
-                    next_row.append(str(getattr(row, field_name)))
-
-                next_file.write('\t'.join(next_row) + '\r\n')
-
-            next_file.close()
-            self.failure('    compare: %s %s' % (model.objects.get_tsv_path(), next_file_name))
+            missing_ids = list(set(original_ids) - set(next_ids))
+            self.failure('    missing_ids: %s' % missing_ids)
