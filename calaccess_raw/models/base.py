bdee03d6b1742a28e13caaeab0147e98b3b03f59
palewire <ben.welsh@gmail.com>
2015-08-20 12:56:04 -0700

    Added a generic global variable on the base model to start the work of #279

--- calaccess_raw/models/base.py
+++ calaccess_raw/models/base.py
@@ -8,6 +8,23 @@ class CalAccessBaseModel(models.Model):
     """
     An abstract model with some tricks we'll reuse below.
     """
+    # The UNIQUE_KEY is one or more fields that, taken together, are unique
+    # within the database. https://en.wikipedia.org/wiki/Unique_key
+
+    # Because the CAL-ACCESS database is released without unique keys specified
+    # we determine these on our own and list them with each model.
+
+    # If a single field is believed to be unique, it can be set as a simple
+    # string variable, like:
+
+    #   UNIQUE_KEY = 'field_name'
+
+    # If multiple fields must be combined to guarantee uniqueness, they
+    # should be listed as tuple like:
+
+    #   UNIQUE_KEY = ('field_one', 'field_two')
+    UNIQUE_KEY = None
+
     objects = managers.CalAccessManager()
 
     def doc(self):
