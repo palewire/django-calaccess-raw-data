8e79bf8f20537f5f25642cde39b4190cd1c21a32
Luciana Godoy <tocateunvals@gmail.com>
2015-08-29 17:11:07 -0300

    Fixed #633

--- calaccess_raw/models/campaign.py
+++ calaccess_raw/models/campaign.py
@@ -3340,7 +3340,10 @@ original filing and 1 to 999 amendments.",
         db_column='FIN_NAMF', max_length=32, blank=True
     )
     fin_namt = fields.CharField(
-        db_column='FIN_NAMT', max_length=32, blank=True
+        db_column='FIN_NAMT', 
+        max_length=32, 
+        blank=True,
+        help_text="Unused. Financial institution's title."
     )
     fin_nams = fields.CharField(
         db_column='FIN_NAMS', max_length=32, blank=True
