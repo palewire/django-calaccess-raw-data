9dfc8e92080f99bdc61779a952f4376496dcfe25
Burt Herman <burtherman@gmail.com>
2015-08-29 14:00:09 -0300

    Added documentation for "acct_no field in F501502Cd database #653

--- calaccess_raw/models/campaign.py
+++ calaccess_raw/models/campaign.py
@@ -3375,7 +3375,7 @@ original filing and 1 to 999 amendments.",
         db_column='CAN_SIG', max_length=13, blank=True
     )
     account_no = fields.CharField(
-        db_column='ACCOUNT_NO', max_length=22, blank=True
+        db_column='ACCOUNT_NO', max_length=22, blank=True, help_text='account number'
     )
     acct_op_dt = fields.DateField(
         db_column='ACCT_OP_DT', blank=True, null=True
