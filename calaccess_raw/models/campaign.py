441cb10b9e32579b78f7b018f0290e1430282d0d
Burt Herman <burtherman@gmail.com>
2015-08-29 13:58:38 -0300

    Added documentation for "acct_op_dt" field in F501502Cd database #654

--- calaccess_raw/models/campaign.py
+++ calaccess_raw/models/campaign.py
@@ -3378,7 +3378,7 @@ original filing and 1 to 999 amendments.",
         db_column='ACCOUNT_NO', max_length=22, blank=True
     )
     acct_op_dt = fields.DateField(
-        db_column='ACCT_OP_DT', blank=True, null=True
+        db_column='ACCT_OP_DT', blank=True, null=True, help_text='account open date'
     )
     party_cd = fields.IntegerField(
         db_column='PARTY_CD', blank=True, null=True
