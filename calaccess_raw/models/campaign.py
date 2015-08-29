e2007b524445bb7bdd58af52108307232639143e
erika <hello@erikaowens.com>
2015-08-29 15:46:58 -0400

    added documentation to the 'elect_data' field

--- calaccess_raw/models/campaign.py
+++ calaccess_raw/models/campaign.py
@@ -754,7 +754,10 @@ original filing and 1 to 999 amendments.",
         max_length=4, db_column='DIST_NO', blank=True
     )
     elect_date = fields.DateTimeField(
-        null=True, db_column='ELECT_DATE', blank=True
+        null=True, 
+	db_column='ELECT_DATE', 
+	blank=True,
+	help_text="Date of the General Election. This date will be the ame as on the filing's cover (CVR) record."
     )
     emplbus_cb = fields.CharField(
         max_length=4, db_column='EMPLBUS_CB', blank=True
