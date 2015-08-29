34f201aa0105f91956d6a44cefc99373f91d031c
Luciana Godoy <tocateunvals@gmail.com>
2015-08-29 14:07:31 -0300

    Fixed #680

--- calaccess_raw/models/campaign.py
+++ calaccess_raw/models/campaign.py
@@ -662,7 +662,10 @@ original filing and 1 to 999 amendments.",
         max_length=40, db_column='BAL_JURIS', blank=True
     )
     bal_name = fields.CharField(
-        max_length=200, db_column='BAL_NAME', blank=True
+        max_length=200, 
+        db_column='BAL_NAME', 
+        blank=True,
+        help_text="Ballot measure name."
     )
     bal_num = fields.CharField(
         max_length=4, db_column='BAL_NUM', blank=True
@@ -3358,7 +3361,7 @@ original filing and 1 to 999 amendments.",
     juris_dscr = fields.CharField(
         db_column='JURIS_DSCR', max_length=14, blank=True
     )
-    dist_no = fields.CharField(db_column='DIST_NO', max_length=4, blank=True, help_text="District number for office being sought. Populated for Senate, Assembly or Board of Equialization races.")
+    dist_no = fields.CharField(db_column='DIST_NO', max_length=4, blank=True)
     party = fields.CharField(db_column='PARTY', max_length=20, blank=True)
     yr_of_elec = fields.IntegerField(
         db_column='YR_OF_ELEC',
