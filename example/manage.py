b9393ca8d285621719cf396d7dc8f82dfeeefea0
James Gordon <gordon.je@gmail.com>
2016-01-21 16:28:16 -0600

    Fixing manage file #1160

--- example/manage.py
+++ example/manage.py
@@ -4,6 +4,6 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-    sys.path.append(os.path.dirname(__file__))
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
