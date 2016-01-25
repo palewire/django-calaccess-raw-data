1a2525c2d172d76277fc8148cf25130093c699d8
palewire <ben.welsh@gmail.com>
2016-01-24 16:08:40 -0800

    Added support for Django's built-in --no-color option to our base management command

--- calaccess_raw/management/commands/__init__.py
+++ calaccess_raw/management/commands/__init__.py
@@ -6,23 +6,24 @@ class CalAccessCommand(BaseCommand):
 
     def handle(self, *args, **options):
         self.verbosity = int(options.get("verbosity"))
+        self.no_color = options.get("no_color")
 
     def header(self, string):
-        self.stdout.write(
-            colorize(string, fg="cyan", opts=("bold",))
-        )
+        if not self.no_color:
+            string = colorize(string, fg="cyan", opts=("bold",))
+        self.stdout.write(string)
 
     def log(self, string):
-        self.stdout.write(
-            colorize("%s" % string, fg="white")
-        )
+        if not self.no_color:
+            string = colorize("%s" % string, fg="white")
+        self.stdout.write(string)
 
     def success(self, string):
-        self.stdout.write(
-            colorize(string, fg="green")
-        )
+        if not self.no_color:
+            string = colorize(string, fg="green")
+        self.stdout.write(string)
 
     def failure(self, string):
-        self.stdout.write(
-            colorize(string, fg="red")
-        )
+        if not self.no_color:
+            string = colorize(string, fg="red")
+        self.stdout.write(string)
