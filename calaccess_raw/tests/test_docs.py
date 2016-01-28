bdb729a99fa24c91773bdf6e7c63b58a8cad1db8
palewire <ben.welsh@gmail.com>
2016-01-27 22:47:51 -0800

    Segemented tests into separate files so we can have our docs stuff out on its own for #1191

--- calaccess_raw/tests/test_docs.py
+++ calaccess_raw/tests/test_docs.py
@@ -7,48 +7,8 @@ from django.conf import settings
 from django.test import TestCase
 from django.db.models import Count
 from calaccess_raw import get_model_list
-from django.test.utils import override_settings
 from django.core.management import call_command
 logger = logging.getLogger(__name__)
-
-
-class UpdateTest(TestCase):
-    """
-    Tests related to the management commands that update the database.
-    """
-    @classmethod
-    def setUpClass(cls):
-        """
-        Load data into the database before running other tests.
-        """
-        super(UpdateTest, cls).setUpClass()
-        kwargs = dict(verbosity=3, test_data=True)
-        if settings.DATABASES.get("alt", None):
-            kwargs['database'] = 'alt'
-            logger.debug("Loading into 'alt' database")
-        call_command("updatecalaccessrawdata", **kwargs)
-
-    @override_settings(BASE_DIR='example/')
-    def test_csv_gettrs(self):
-        """
-        Verify that get_csv_name methods work for all models.
-        """
-        for m in get_model_list():
-            self.assertEqual(m.objects.get_csv_name(), m().get_csv_name())
-            self.assertEqual(m.objects.get_csv_path(), m().get_csv_path())
-
-    def test_verifycalacessrawfile(self):
-        """
-        Test that verifycalaccessrawfile management command is working.
-        """
-        call_command("verifycalaccessrawfile", 'RcptCd')
-        call_command("verifycalaccessrawfile", 'AcronymsCd')
-
-    def test_totalcalacessrawdata(self):
-        """
-        Test that verifycalaccessrawfile management command is working.
-        """
-        call_command("totalcalaccessrawdata")
 
 
 class DocumentationTest(TestCase):
