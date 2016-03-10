97cb5da2aa9c2e4e5f47f55ced3e34bc26eccab8
Sandhya Kambhampati <sjkambhampati@gmail.com>
2016-03-10 15:51:26 -0700

    Updated file for lexp
    Added in pages for documentcloud

--- calaccess_raw/models/lobbying.py
+++ calaccess_raw/models/lobbying.py
@@ -1813,6 +1813,9 @@ class LexpCd(CalAccessBaseModel):
         "REC_TYPE",
         "FORM_TYPE"
     )
+    DOCUMENTCLOUD_PAGES = [
+        DocumentCloud(id='2711614-CalAccessTablesWeb', start_page=86, end_page=87),
+    ]
     amend_id = fields.IntegerField(
         db_column='AMEND_ID',
         db_index=True,
