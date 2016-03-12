00a2722e6ab2289f1b0e330907cba3ab69fdfbf7
Michael Keller <mhkeller@users.noreply.github.com>
2016-03-12 14:56:02 -0700

    Add doc cloud reference to LobbyistFirmLobbyist1Cd
    Re: https://github.com/california-civic-data-coalition/django-calaccess-raw-data/issues/1209

--- calaccess_raw/models/lobbying.py
+++ calaccess_raw/models/lobbying.py
@@ -3740,6 +3740,9 @@ class LobbyistFirmLobbyist1Cd(CalAccessBaseModel):
     It's an undocumented model.
     """
     UNIQUE_KEY = False
+    DOCUMENTCLOUD_PAGES = [
+        DocumentCloud(id='2711614-CalAccessTablesWeb', start_page=102),
+    ]
     lobbyist_id = fields.IntegerField(
         db_column='LOBBYIST_ID',
         verbose_name="Lobbyist ID",
