993282b27129b4799f00f7b596577db6cf01ccc0
Steven Rich <steven.rich@washpost.com>
2016-03-12 16:58:48 -0500

    Added documentation for the S497Cd field

--- calaccess_raw/models/campaign.py
+++ calaccess_raw/models/campaign.py
@@ -4572,6 +4572,9 @@ class S497Cd(CalAccessBaseModel):
         "REC_TYPE",
         "FORM_TYPE"
     )
+    DOCUMENTCLOUD_PAGES = [
+        DocumentCloud(id=2711614, start_page=125, end_page=127),
+    ]
     filing_id = fields.IntegerField(
         db_column='FILING_ID',
         db_index=True,
