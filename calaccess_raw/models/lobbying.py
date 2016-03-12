6a63f20604a5bd43ea26e4e365f2a09a86cb17fc
Dan Hill <danhillreports@gmail.com>
2016-03-12 16:17:18 -0700

    Update CvrLobbyDisclosureCd DocumentCloud docs

--- calaccess_raw/models/lobbying.py
+++ calaccess_raw/models/lobbying.py
@@ -731,7 +731,8 @@ class CvrLobbyDisclosureCd(CalAccessBaseModel):
         "FORM_TYPE"
     )
     DOCUMENTCLOUD_PAGES = [
-        DocumentCloud(id=2711614, start_page=42, end_page=44),
+        DocumentCloud(id='2711614-CalAccessTablesWeb', start_page=32, end_page=35),
+        DocumentCloud(id='2711616-MapCalFormat2Fields', start_page=17, end_page=21),
     ]
     amend_id = fields.IntegerField(
         db_column='AMEND_ID',
