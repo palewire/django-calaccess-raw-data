1bfce154c1033f272de3082cd1c18de44ab567a5
James Gordon <gordon.je@gmail.com>
2016-03-12 17:37:52 -0700

    Update campaign.py

--- calaccess_raw/models/campaign.py
+++ calaccess_raw/models/campaign.py
@@ -1505,6 +1505,7 @@ class Cvr2CampaignDisclosureCd(CalAccessBaseModel):
     )
     DOCUMENTCLOUD_PAGES = [
         DocumentCloud(id='2711614-CalAccessTablesWeb', start_page=41, end_page=43),
+        DocumentCloud(id='2711614-CalAccessTablesWeb', start_page=8),
     ]
     amend_id = fields.IntegerField(
         db_column='AMEND_ID',
