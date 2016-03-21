877cc2474520afe9e31d4da9eafcc8a536a38f1a
James Gordon <gordon.je@gmail.com>
2016-03-21 11:09:37 -0500

    Add call to downloadcalaccessrawdatatest, remove unnecessary if conditions around test mode and tracking models #1314

--- calaccess_raw/management/commands/updatecalaccessrawdata.py
+++ calaccess_raw/management/commands/updatecalaccessrawdata.py
@@ -88,16 +88,16 @@ class Command(CalAccessCommand):
         self.downloading = options['download']
         self.cleaning = options['clean']
         self.loading = options['load']
+        self.noinput = options['noinput']
 
         if self.test_mode:
-            # if using test data, we don't need to download
-            self.downloading = False
             # and always keep files when running test data
             self.keep_files = True
             self.data_dir = get_test_download_directory()
             # need to set this app-wide because cleancalaccessrawfile
             #   also calls get_download_directory
             settings.CALACCESS_DOWNLOAD_DIR = self.data_dir
+            self.noinput = True
         else:
             self.data_dir = get_download_directory()
 
@@ -118,15 +118,24 @@ class Command(CalAccessCommand):
         os.path.exists(self.csv_dir) or os.makedirs(self.csv_dir)
 
         download_metadata = self.get_download_metadata()
-        current_release_datetime = download_metadata['last-modified']
+        
+        if self.test_mode: 
+            with open(self.data_dir + "/sampled_version.txt", "r") as f:
+                current_release_datetime = f.readline()    
+        else:
+            current_release_datetime = download_metadata['last-modified']
+        
         last_started_update = self.get_last_log()
 
-        try:
-            last_download = self.command_logs.filter(
-                command='downloadcalaccessrawdata'
-            ).order_by('-start_datetime')[0]
-        except IndexError:
+        if self.test_mode:
             last_download = None
+        else:
+            try:
+                last_download = self.command_logs.filter(
+                    command='downloadcalaccessrawdata'
+                ).order_by('-start_datetime')[0]
+            except IndexError:
+                last_download = None
 
         up_to_date = False
         can_resume = False
@@ -155,7 +164,7 @@ class Command(CalAccessCommand):
                         if last_download.finish_datetime:
                             can_resume = True
 
-        if options['noinput']:
+        if self.noinput:
             # if not taking input and can resume, automatically go into resume mode
             self.resume_mode = can_resume
         else:
@@ -191,27 +200,26 @@ class Command(CalAccessCommand):
                 if not self.confirm_proceed(prompt):
                     raise CommandError("Update cancelled")
 
-        if not self.test_mode:
-            if self.resume_mode:
-                self.log_record = last_started_update
-            else:
-                # get or create a version
-                # .get_or_create() throws IntegrityError
-                try:
-                    version = self.raw_data_versions.get(
-                        release_datetime=current_release_datetime
-                    )
-                except RawDataVersion.DoesNotExist:
-                    version = self.raw_data_versions.create(
-                        release_datetime=current_release_datetime,
-                        size=download_metadata['content-length']
-                    )
-                # create a new log record
-                self.log_record = self.command_logs.create(
-                    version=version,
-                    command=self,
-                    called_by=self.get_caller_log()
+        if self.resume_mode:
+            self.log_record = last_started_update
+        else:
+            # get or create a version
+            # .get_or_create() throws IntegrityError
+            try:
+                version = self.raw_data_versions.get(
+                    release_datetime=current_release_datetime
                 )
+            except RawDataVersion.DoesNotExist:
+                version = self.raw_data_versions.create(
+                    release_datetime=current_release_datetime,
+                    size=download_metadata['content-length']
+                )
+            # create a new log record
+            self.log_record = self.command_logs.create(
+                version=version,
+                command=self,
+                called_by=self.get_caller_log()
+            )
 
         # if the user could have resumed but didn't
         force_restart_download = can_resume and not self.resume_mode
@@ -228,13 +236,19 @@ class Command(CalAccessCommand):
                         self.downloading = False
 
         if self.downloading:
-            call_command(
-                "downloadcalaccessrawdata",
-                keep_files=self.keep_files,
-                verbosity=self.verbosity,
-                noinput=True,
-                restart=force_restart_download,
-            )
+            if self.test_mode:
+                call_command(
+                    "downloadcalaccessrawdatatest",
+                    verbosity=self.verbosity,
+                )
+            else:
+                call_command(
+                    "downloadcalaccessrawdata",
+                    keep_files=self.keep_files,
+                    verbosity=self.verbosity,
+                    noinput=True,
+                    restart=force_restart_download,
+                )
             if self.verbosity:
                 self.duration()
 
@@ -251,10 +265,9 @@ class Command(CalAccessCommand):
 
         if self.verbosity:
             self.success("Done!")
-
-        if not self.test_mode:
-            self.log_record.finish_datetime = datetime.now()
-            self.log_record.save()
+        
+        self.log_record.finish_datetime = datetime.now()
+        self.log_record.save()
 
     def clean(self):
         """
