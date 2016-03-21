b4002113573b7f7fa00dce19eacd5566c96d12a1
James Gordon <gordon.je@gmail.com>
2016-03-21 14:58:36 -0500

    Fix conf file, remove old stylesheet

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_style = 'css/docs_style.css'
+html_style = 'css/custom.css'
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
