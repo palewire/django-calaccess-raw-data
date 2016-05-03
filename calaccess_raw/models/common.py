8abf62497708fb3dc50c820d7c27a17e8195f800
James Gordon <gordon.je@gmail.com>
2016-05-03 15:51:12 -0500

    Add missing imports

--- calaccess_raw/models/common.py
+++ calaccess_raw/models/common.py
@@ -3,7 +3,11 @@
 from __future__ import unicode_literals
 from .base import CalAccessBaseModel
 from calaccess_raw import fields
-from calaccess_raw.annotations import DocumentCloud
+from calaccess_raw.annotations import (
+    DocumentCloud,
+    choices,
+    get_sorted_choices
+)
 from calaccess_raw.annotations.filing_forms import get_filing_form, all_filing_forms
 from django.template.defaultfilters import floatformat
 from django.utils.encoding import python_2_unicode_compatible
