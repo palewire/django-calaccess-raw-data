4146b1d3ab80b5ea9e5fda06687c4f456afddada
James Gordon <gordon.je@gmail.com>
2016-05-27 13:26:40 -0500

    Patch for 1.0.0 release, remove deco import

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-calaccess-raw-data',
-    version='1.0.0',
+    version='1.0.1',
     packages=(
         'calaccess_raw',
         'calaccess_raw.admin',
