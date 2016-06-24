0bea4bf6fb5899ca3f67f26982fd2d1beaf4637b
palewire <ben.welsh@gmail.com>
2016-06-24 07:25:13 -0700

    Added tests to get fields coverage up to 100%

--- calaccess_raw/tests/test_fields.py
+++ calaccess_raw/tests/test_fields.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 import logging
 from calaccess_raw import fields
+from calaccess_raw import models
 from django.test import TestCase
 logger = logging.getLogger(__name__)
 
@@ -12,10 +13,22 @@ class FieldTestCase(TestCase):
     Tests related to our custom database fields.
     """
     multi_db = True
-    
+
     def test_fields(self):
         f = fields.CharField(help_text="foo")
         self.assertEqual(f.definition(), "Foo")
         f2 = fields.CharField()
         self.assertEqual(f2.definition(), "")
 
+        m = models.CvrCampaignDisclosureCd()
+        f3 = m.get_field_list()[1]
+        f3.is_unique_key()
+        f4 = m.get_field_list()[2]
+        f4.is_unique_key()
+        f4.description()
+
+        m2 = models.FilerToFilerTypeCd()
+        f5 = m2.get_field_list()[2]
+        f5.copy_type
+        f5.copy_template
+        f5.description()
