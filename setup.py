eba87a895a947a909b88435479ca64e567dfac6f
James Gordon <gordon.je@gmail.com>
2016-07-07 16:01:53 -0600

    Release v1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-calaccess-raw-data',
-    version='1.2.0',
+    version='1.3.0',
     packages=(
         'calaccess_raw',
         'calaccess_raw.admin',
