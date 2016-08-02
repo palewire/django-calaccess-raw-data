c57d27210436b064bfd9e7c14f2fccc622122ab3
James Gordon <gordon.je@gmail.com>
2016-08-02 13:55:33 -0500

    Add humanize to INSTALLED_APPS

--- example/settings.py
+++ example/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'toolbox',
     'calaccess_raw',
 )
