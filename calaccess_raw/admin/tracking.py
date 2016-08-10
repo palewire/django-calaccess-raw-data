31adc2cfd6822f381cab31cbce7a22ec9743bf29
palewire <ben.welsh@gmail.com>
2016-08-09 23:56:01 -0700

    Added new methods for verison admin with file and record count

--- calaccess_raw/admin/tracking.py
+++ calaccess_raw/admin/tracking.py
@@ -19,6 +19,8 @@ class RawDataVersionAdmin(BaseAdmin):
         "release_datetime",
         "pretty_download_size",
         "pretty_clean_size",
+        "file_count",
+        "record_count"
     )
     list_display_links = ('release_datetime',)
     list_filter = ("release_datetime",)
@@ -36,12 +38,7 @@ class RawDataFileAdmin(BaseAdmin):
         "download_records_count",
         "clean_records_count",
         "load_records_count",
-        "error_count",
-        "download_columns_count",
-        "clean_columns_count",
-        "load_columns_count",
-        "pretty_download_file_size",
-        "pretty_clean_file_size"
+        "error_count"
     )
     list_display_links = ('id', 'file_name',)
     list_filter = ("version__release_datetime",)
