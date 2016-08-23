0394e0065a8cd96696cc86573c8b2853c19b2afb
James Gordon <gordon.je@gmail.com>
2016-08-23 09:23:45 -0500

    Consistently decode tsv_lines as ascii (w/ replace)

--- calaccess_raw/management/commands/cleancalaccessrawfile.py
+++ calaccess_raw/management/commands/cleancalaccessrawfile.py
@@ -144,8 +144,8 @@ class Command(CalAccessCommand):
             line_number += 1
             # Log empty lines, then skip
             if (
-                tsv_line.decode('utf8') == '\n' or
-                tsv_line.decode('utf8') == '\r\n'
+                tsv_line.decode("ascii", "replace") == '\n' or
+                tsv_line.decode("ascii", "replace") == '\r\n'
             ):
                 if self.verbosity > 2:
                     msg = '  Line %s is empty'
