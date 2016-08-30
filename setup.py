8812091d6b0d7c55a1ba62cfb0f95604560c0bed
palewire <ben.welsh@gmail.com>
2016-08-29 21:20:37 -0700

    Upgraded to version 1.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-calaccess-raw-data',
-    version='1.4.1',
+    version='1.4.2',
     packages=(
         'calaccess_raw',
         'calaccess_raw.admin',
