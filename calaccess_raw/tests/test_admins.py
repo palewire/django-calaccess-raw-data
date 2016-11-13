f1d9cd25cfa27e37a2b14b195d2bf5ccd97bc456
palewire <ben.welsh@gmail.com>
2016-11-13 12:07:00 -0800

    More admin tests to increase coverage

--- calaccess_raw/tests/test_admins.py
+++ calaccess_raw/tests/test_admins.py
@@ -6,6 +6,7 @@ Tests the adminstration panel configuration.
 from __future__ import unicode_literals
 from django.test import TestCase
 from calaccess_raw import admin
+from calaccess_raw import models
 from calaccess_raw import get_model_list
 
 
@@ -21,3 +22,9 @@ class AdminTestCase(TestCase):
         admin_list = [a.replace("Admin", "") for a in admin.__all__]
         missing = set(model_list).difference(admin_list)
         self.assertEqual(missing, set([]))
+
+    def test_methods(self):
+        a = admin.CvrSoCdAdmin(models.CvrSoCd(), None)
+        a.get_readonly_fields()
+        a.get_list_filter({})
+        a.get_search_fields({})
