7cb64348e277584135dcd9af58466b50ade74545
palewire <ben.welsh@gmail.com>
2017-04-18 09:14:38 -0700

    Fixes #1500

--- example/urls.py
+++ example/urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
+from django.conf.urls import url
 from django.views.static import serve
-from django.conf.urls import include, url
 admin.autodiscover()
 
 
 urlpatterns = (
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^static/(?P<path>.*)$', serve, {
         'document_root': settings.STATIC_ROOT,
         'show_indexes': True,
