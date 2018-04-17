b9545b7dd39157cb3a3dbeda5176d743d57da9b3
palewire <ben.welsh@gmail.com>
2018-04-16 17:31:00 -0700

    v1.6.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-calaccess-raw-data',
-    version='1.6.2',
+    version='1.6.3',
     packages=(
         'calaccess_raw',
         'calaccess_raw.admin',
