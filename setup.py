1fe72b9da5c897d83d2f46e3ed5ee48df6e24e1d
palewire <b@palewi.re>
2021-06-19 04:17:14 -0700

    v4.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='django-calaccess-raw-data',
-    version='3.1.2',
+    version='4.0.0',
     author='California Civic Data Coalition',
     author_email='b@palewi.re',
     url='http://django-calaccess.californiacivicdata.org/',
