127254a7150b28f3fed785f78d5b8b2b81a41094
palewire <b@palewi.re>
2022-09-09 16:58:05 -0700

    Django 4.0 upgrade

--- example/urls.py
+++ example/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
-from django.conf.urls import url
+from django.urls import re_path
 from django.views.static import serve
 admin.autodiscover()
 
 
 urlpatterns = (
-    url(r'^admin/', admin.site.urls),
-    url(r'^static/(?P<path>.*)$', serve, {
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^static/(?P<path>.*)$', serve, {
         'document_root': settings.STATIC_ROOT,
         'show_indexes': True,
     }),
